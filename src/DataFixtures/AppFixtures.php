04326c80bb6923a2cc29aec6199410138b34dce1
Francescx <Francesco2023@skiff.com>
2023-09-14 16:24:44 +0200

    En Train de Rajouter les Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,34 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\User;
+
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->email())
+                ->setPassword($this->faker->word())
+                ->setPseudo($this->faker->word())
+                ->setNom($this->faker->word())
+                ->setPrenom($this->faker->word())
+                ->setTelephone($this->faker->phoneNumber());
+
+            
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
