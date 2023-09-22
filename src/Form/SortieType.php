2fc8a53f0b871f8f348823dcc9c0f3e23e5609c1
Francescx <Francesco2023@skiff.com>
2023-09-22 13:02:03 +0200

    finito

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,6 +6,7 @@ use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,12 +17,56 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, ['label' => 'Nom'])
-            ->add('dateHeureDebut', DateTimeType::class, ['label' => 'Date et heure de début'])
-            ->add('Duree', TimeType::class, ['label' => ' Durée'])
-            ->add('dateLimiteInscription', DateTimeType::class, ['label' => 'Date limite d\'inscription'])
-            ->add('nombInscripMax', IntegerType::class, ['label' => ' Nombre d\'inscription maximum'])
-            ->add('infoSortie', TextType::class, ['label'=> 'Infos sortie'])
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('dateHeureDebut', DateTimeType::class,  [
+                'html5' => true,
+                'widget' => 'single_text',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+                
+            ->add('Duree', TimeType::class, [
+                'label' => ' Durée',
+                'html5' => true,
+                'widget' => 'single_text',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Date limite d\'inscription',
+                'html5' => true,
+                'widget' => 'single_text',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+                ])
+            ->add('nombInscripMax', IntegerType::class, [
+                'label' => ' Nombre d\'inscription maximum',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            
+            ])
+            ->add('infoSortie', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
         ;
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,14 +15,35 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, ['label' => 'Email : '])
-            ->add('roles')
-            ->add('password', PasswordType::class, ['label' => 'Password'])
-            ->add('prénom', TextType::class, ['label' => 'Prénom'])
-            ->add('nom', TextType::class, ['label' => 'Nom'])
-            ->add('telephone', TextType::class, ['label' => 'Téléphone : '])
-            ->add('administrateur')
-            ->add('actif')
+            
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prenom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Telephone',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            
         ;
     }
 
